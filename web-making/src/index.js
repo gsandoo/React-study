3f7958efd507614889f83c1afe899927248e10cb
gsandoo <rogersddt@naver.com>
2022-05-26 22:30:32 +0900

    axios practice

--- web-making/src/index.js
+++ web-making/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Test from './components/Test';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-      <App />
+      <Test/>
     </BrowserRouter>
 );
 
